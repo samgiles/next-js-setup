dac10705a512a874bac59984aee1da428340871b
Rhys Evans <rhys.evans@ft.com>
2015-03-27 12:34:15 +0000

    granualr selection of features to boostrap and allowing for asynchronous actions in app setup

--- src/js-setup.js
+++ src/js-setup.js
@@ -7,6 +7,26 @@ var userPrefs = require('next-user-preferences');
 var beacon = require('next-beacon-component');
 var welcome = require('next-welcome');
 
+function extendWithCoreFeatures (obj) {
+	Object.keys(coreFeatures).forEach(function (key) {
+		obj[key] = true;
+	});
+}
+
+var coreFeatures = {
+	personalisedContentApi: true, //TODO remove from core
+	raven: true,
+	beacon: true
+};
+
+var wrapperFeatures = extendWithCoreFeatures({
+	welcome: true,
+	header: true,
+	footer: true,
+	personalisedContentUi: true
+});
+
+
 var JsSetup = function () {};
 
 JsSetup.noop = function () {};
@@ -15,6 +35,9 @@ JsSetup.prototype.init = function (opts) {
 
 	// may be used for app specific config in future
 	opts = opts || {};
+
+	var features = opts.features === 'wrapper' 			? wrapperFeatures :
+								typeof opts.features === 'object' ? extendWithCoreFeatures(opts.features) : coreFeatures;
 
 	try {
 		flags.setUrl(opts.flagsUrl || '/__flags.json');
@@ -28,7 +51,7 @@ JsSetup.prototype.init = function (opts) {
 
 	return flags.init().then(function() {
 
-		if (flags.get('clientErrorReporting').isSwitchedOn) {
+		if (features.raven && flags.get('clientErrorReporting').isSwitchedOn) {
 			Raven.config('https://edb56e86be2446eda092e69732d8654b@app.getsentry.com/32594').install();
 
 			// normalise client and server side method names
@@ -39,17 +62,30 @@ JsSetup.prototype.init = function (opts) {
 
 		this.raven = Raven;
 
-		if (flags.get('userPreferencesAPI').isSwitchedOn) {
+		if (features.personalisedContentApi && flags.get('userPreferencesAPI').isSwitchedOn) {
 			var userPrefsOpts = opts.userPreferences || {};
 			userPrefsOpts.flags = flags;
 			userPrefs.init(userPrefsOpts);
 		}
 
-		if (flags.get('beacon').isSwitchedOn) {
+		if (features.beacon && flags.get('beacon').isSwitchedOn) {
 			beacon.init && beacon.init(opts.beacon);
 		}
 
-		if (flags.get('welcomePanel').isSwitchedOn) {
+		if (features.header) {
+			header.init();
+		}
+
+		if (features.footer) {
+			footer.init();
+		}
+
+		// doesn't exist yet - bundled in with user prefs
+		// if (features.personalisedContentUi) {
+		// 	personalisedContentUi.init();
+		// }
+
+		if (features.welcome && flags.get('welcomePanel').isSwitchedOn) {
 			welcome.init();
 		}
 
@@ -61,10 +97,18 @@ JsSetup.prototype.init = function (opts) {
 
 JsSetup.prototype.bootstrap = function (callback, opts) {
 
+	var success = function () {
+		document.documentElement.classList.add('js-success');
+	};
+
 	var go = function () {
 		this.bootstrapResult = this.init(opts).then(function (result) {
-			callback(result);
-			document.querySelector('html').classList.add('js-success');
+			var promise = callback(result);
+			if (promise && typeof promise.then === 'function') {
+				promise.then(success);
+			} else {
+				success();
+			}
 		});
 	}.bind(this);
 
